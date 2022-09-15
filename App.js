a4fca57b4c6859c9299c1ff9c64f634bc8cc1162
Ung Hung <cusn1802@gmail.com>
2022-09-15 14:17:01 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      alignItems: 'center',
+      flexDirection: 'row',
+      flex: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: "#dddddd",
+      fontWeight: 500
+    }}>
+      <View style={{ textAlign: 'center' }}>
+        <View style={styles.InputView}>
+          <input style={styles.TextInput} type='text' placeholder='Email' />
+        </View>
+        <View style={styles.InputView}>
+          <input style={styles.TextInput} type='password' placeholder='Password' />
+        </View>
+        <TouchableOpacity style={{ padding: 10, backgroundColor: '#00b6ed', borderRadius: 30, marginBottom: 20 }}>
+          <Text style={{ color: '#fff', textAlign: 'center' }}>Login</Text>
+        </TouchableOpacity>
+        <a href='/register' style={{ textDecoration: 'none', color: '#000', marginBottom: 40 }}>Forgot your password?</a>
+        <a href='/register' style={{ textDecoration: 'none', color: '#000' }}>Register</a>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  InputView: {
+    display: 'flex',
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
   },
-});
+  TextInput: {
+    width: 300,
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    height: '100%',
+    padding: 10,
+    paddingLeft: 60,
+    border: 'none',
+    fontWeight: 600,
+  }
+}
+);
